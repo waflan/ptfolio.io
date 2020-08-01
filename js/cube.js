fabace2da4df1447e8b3d8b74824e5274dd02e8a
x19053 <x19053xx@aitech.ac.jp>
2020-08-01 12:41:21 +0900

    トップページ立方体完成

--- js/cube.js
+++ js/cube.js
@@ -1,3 +1,4 @@
+// 立方体を生成
 function cube(size,element) {
     let d = size / 2;
     let parent = element;
@@ -6,10 +7,12 @@ function cube(size,element) {
     parent.style.transform = "translate3d(100px,100px,0)";
     for (let i = 0; i < 6; i++) {
         let tile = document.createElement("div");
+        tile.style.border = "2px solid var(--bg-color-1)";
+        tile.style.opacity='0.5';
         tile.style.position = "absolute";
         tile.style.height = size+"px";
         tile.style.width =size+"px";
-        tile.style.backgroundColor="hsl("+(i*60)+", 100%, 50%)";
+        tile.style.backgroundColor="hsl(160, 20%, "+ (i*10+50) +"%)";
         parent.appendChild(tile);
     }
     let tiles = parent.children;
@@ -21,12 +24,28 @@ function cube(size,element) {
     tiles[5].style.transform = "translate3d(0,0,-"+ d +"px) rotateY(180deg)";
 };
 let cube1 =document.getElementById("cube");
+cube1.style.transition = '0';
 var rotx=0;
 var roty=0;
+var scale= 1.6;
 cube(100,cube1);
+// 立方体を回転させる
 function rot() {
     rotx+=0.56;
     roty+=1.3;
-    cube1.style.transform = "translate3d(100px,100px,0) rotateX("+rotx+"deg) rotateY("+roty+"deg)";
+    cube1.style.transform = "rotateX("+rotx+"deg) rotateY("+roty+"deg) scale3d("+scale+","+scale+","+scale+")";
 }
 setInterval(rot,50);
+
+let cubeParent = document.getElementById("cubeParent");
+// 立方体にホバーしたときの動作
+cube1.addEventListener("mouseover",function() {
+    rotx+=13;
+    roty+=25;
+    scale = 2;
+    cube1.style.transition = '0.2s';
+});
+cube1.addEventListener("mouseout",function() {
+    scale=1.6;
+    cube1.style.transition = '0';
+});
